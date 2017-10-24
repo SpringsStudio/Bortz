a047d552be7eee451a89b3a9fe1be7bd190feeed
Ashymad <czilukim@o2.pl>
2017-10-24 16:46:18 +0200

    Create training menu layout

--- code/app/src/main/java/studio/springs/bortz/MainMenu.java
+++ code/app/src/main/java/studio/springs/bortz/MainMenu.java
@@ -1,7 +1,11 @@
 package studio.springs.bortz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        final Button upButton = (Button) findViewById(R.id.buttonUp);
+        upButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Not Implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public void goToTraining(View view) {
+        Intent train = new Intent(this, TrainingMenu.class);
+        startActivity(train);
     }
 }
---
+++ code/app/src/main/java/studio/springs/bortz/TrainingMenu.java
@@ -0,0 +1,16 @@
+package studio.springs.bortz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TrainingMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_menu);
+
+
+    }
+}
