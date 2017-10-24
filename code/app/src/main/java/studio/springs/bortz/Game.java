b2afc5e613b8296e2acef00d007573b882e1f934
rotypo <marcel.piszak@wp.pl>
2017-10-24 17:40:56 +0200

    added game activity

---
+++ code/app/src/main/java/studio/springs/bortz/Game.java
@@ -0,0 +1,16 @@
+package studio.springs.bortz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marcel on 24.10.17.
+ */
+
+public class Game extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
