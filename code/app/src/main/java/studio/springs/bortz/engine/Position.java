abdc3bf76cfaa0a8583244b14b4c8965afe1d278
Ashymad <czilukim@o2.pl>
2017-10-30 11:28:45 +0100

    Make position immutable

--- code/app/src/main/java/studio/springs/bortz/engine/Position.java
+++ code/app/src/main/java/studio/springs/bortz/engine/Position.java
@@ -1,8 +1,8 @@
 package studio.springs.bortz.engine;
 
 public class Position {
-    public int x;
-    public int y;
+    final public int x;
+    final public int y;
 
     public Position(int x, int y){
         this.x = x;
@@ -10,10 +10,6 @@ public class Position {
     }
     public boolean Equals(Position other) {
         return other.x == x && other.y == y;
-    }
-    public void Subtract(Position other){
-        x -= other.x;
-        y -= other.y;
     }
     public static Position Subtract(Position one, Position two){
         return new Position(one.x - two.x, one.y - two.y);
