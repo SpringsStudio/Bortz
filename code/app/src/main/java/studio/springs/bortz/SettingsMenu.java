ff31bdd99cc3848103f7c4b6de53dd5f18a04930
rotypo <marcel.piszak@wp.pl>
2017-11-05 11:58:08 +0100

    Added some stuff to SettingsMenu

--- code/app/src/main/java/studio/springs/bortz/SettingsMenu.java
+++ code/app/src/main/java/studio/springs/bortz/SettingsMenu.java
@@ -1,16 +1,27 @@
 package studio.springs.bortz;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsMenu extends AppCompatActivity {
+public class SettingsMenu extends AppCompatActivity implements AdapterView.OnItemClickListener, CompoundButton.OnCheckedChangeListener {
 
     private ThemeManager themeManager = new ThemeManager();
+    ListView listView;
+    Switch guidesSwitch;
+    SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+    SharedPreferences.Editor editor = pref.edit();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +32,30 @@ public class SettingsMenu extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, themeNames);
 
-        ListView listView = (ListView) findViewById(R.id.listView1);
-        listView.setAdapter(adapter);
+        guidesSwitch = (Switch) findViewById(R.id.switch1);
+        guidesSwitch.setOnCheckedChangeListener(this);
 
+        listView = (ListView) findViewById(R.id.listView1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+
+
+
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id ){
+        TextView text = (TextView) view;
+        Toast.makeText(this, text.getText() ,Toast.LENGTH_LONG).show();
+        listView.setSelector(android.R.color.darker_gray);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton  buttonView, boolean isChecked){
+        if (isChecked)
+            Toast.makeText(this, "guides enabled" ,Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "guides disabled" ,Toast.LENGTH_LONG).show();
     }
 
 }
