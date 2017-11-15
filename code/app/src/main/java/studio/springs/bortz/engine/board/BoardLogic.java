8a4f18a2bff5da0d2ad3931ed713a6ecd68347ab
rotypo <marcel.piszak@wp.pl>
2017-11-15 20:48:19 +0100

    BoardLogic class code refactored

--- code/app/src/main/java/studio/springs/bortz/engine/board/BoardLogic.java
+++ code/app/src/main/java/studio/springs/bortz/engine/board/BoardLogic.java
@@ -34,52 +34,12 @@ public class BoardLogic {
     public void movePiece(Position from, Position to) throws IllegalMoveException {
         final GamePiece toPiece = board.getPiece(to);
         final GamePiece fromPiece = board.getPiece(from);
-        boolean gameWon = false;
-        if(fromPiece.getColor() != getPlayerColor()) {
+        if(isNowOpponentsTurn(fromPiece)) {
             throw new IllegalMoveException("It's not your turn right now!");
-        } else if (toPiece != null && fromPiece.getColor() == toPiece.getColor()) {
+        } else if (isOpponentsPieceChosen(fromPiece, toPiece)) {
             throw new IllegalMoveException("You cannot capture your own piece.");
         } else if (fromPiece.canMove(Position.Subtract(to, from))) {
-            if (toPiece != null) {
-                if (toPiece.getType() == PieceType.LION) {
-                    gameWon = true;
-                }
-                else {
-                    GamePiece demotedPiece = toPiece.demote();
-                    GamePiece capturedPiece = demotedPiece == null ? toPiece : demotedPiece;
-
-                    board.addCapturedPiece(capturedPiece.swapColor());
-                }
-            }
-            // If white lion reaches the end or black lion reaches beginning check if they are in danger, if not win.
-            if (fromPiece.getType() == PieceType.LION && (
-                    fromPiece.getColor() == PieceColor.BLACK && to.y == 0 ||
-                    fromPiece.getColor() == PieceColor.WHITE && to.y == board.getSize().y - 1)){
-                gameWon = true;
-                for (int xi = -1; xi < 2; xi++){
-                    for (int yi = -1; yi < 2; yi++){
-                        GamePiece checkPiece = board.getPiece(Position.Add(to, new Position(xi,yi)));
-                        if (checkPiece != null && fromPiece.getColor() != checkPiece.getColor() &&
-                                checkPiece.canMove(new Position(-xi, -yi))){
-                            gameWon = false;
-                            break;
-                        }
-                    }
-                }
-            }
-            // Promote piece if it reaches the end or the beginning of the board.
-            if (((to.y == board.getSize().y - 1 && fromPiece.getColor() == PieceColor.WHITE ) ||
-                    (to.y == 0 && fromPiece.getColor() == PieceColor.BLACK)) &&
-                    fromPiece.promote() != null){
-                board.setPiece(to, fromPiece.promote());
-            }
-            else {
-                board.setPiece(to, fromPiece);
-            }
-            board.setPiece(from, null);
-
-            record.add(new GameMove(fromPiece.getType(),from, GameMove.MoveType.SIMPLE_MOVEMENT,to));
-            if (gameWon) board.changes.add(new BoardChange(BoardChange.ChangeType.WIN, new Position(-1,-1), fromPiece));
+            updateGameStatus(toPiece, fromPiece, from, to);
         } else throw new IllegalMoveException("This piece cannot move like this");
     }
     PieceColor getPlayerColor(){
@@ -87,10 +47,10 @@ public class BoardLogic {
     }
     public void dropPiece(Position pos, PieceType type) throws IllegalMoveException {
         GamePiece piece = GamePieceFactory.createPiece(type,getPlayerColor());
-        if (board.getPiece(pos) != null) {
+        if (isPositionAlreadyTaken(pos)) {
             throw new IllegalMoveException("A piece cannot be placed on a square that is already taken");
         }
-        else if (board.countCapturedPieces(piece) < 1){
+        else if (isThisPieceAvailable(piece)){
             throw new IllegalMoveException("You do not have this type of piece captured");
         }
         else {
@@ -102,4 +62,88 @@ public class BoardLogic {
     public List<GameMove> getMoves(){
         return record;
     }
+
+    private void updateGameStatus(GamePiece toPiece, GamePiece fromPiece, Position from, Position to){
+        boolean gameWon = false;
+        if (isPieceAttacked(toPiece)) {
+            gameWon = destroyPieceOrWin(toPiece);
+        }
+        // If white lion reaches the end or black lion reaches beginning check if they are in danger, if not win.
+        if (isLionReachingEndOfBoard(fromPiece, to)){
+            if (isLionWinningSecurely(fromPiece, to)) gameWon = true;
+        }
+        // Promote piece if it reaches the end or the beginning of the board.
+        if (isPieceReachningEndOfBoardAndNotPromoted(fromPiece, to)){
+            board.setPiece(to, fromPiece.promote());
+        }
+        else {
+            board.setPiece(to, fromPiece);
+        }
+        board.setPiece(from, null);
+
+        record.add(new GameMove(fromPiece.getType(),from, GameMove.MoveType.SIMPLE_MOVEMENT,to));
+        if (gameWon) board.changes.add(new BoardChange(BoardChange.ChangeType.WIN, new Position(-1,-1), fromPiece));
+    }
+
+    private boolean destroyPieceOrWin(GamePiece toPiece){
+        if (toPiece.getType() == PieceType.LION) {
+            return true;
+        }
+        else {
+            GamePiece demotedPiece = toPiece.demote();
+            GamePiece capturedPiece = demotedPiece == null ? toPiece : demotedPiece;
+
+            board.addCapturedPiece(capturedPiece.swapColor());
+            return false;
+        }
+    }
+
+    private boolean isLionWinningSecurely(GamePiece fromPiece ,Position to){
+        for (int xi = -1; xi < 2; xi++){
+            for (int yi = -1; yi < 2; yi++){
+                GamePiece checkPiece = board.getPiece(Position.Add(to, new Position(xi,yi)));
+                if (isLionInDangerFromParticularPiece(checkPiece, fromPiece, xi, yi)){
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+    private boolean isLionInDangerFromParticularPiece(GamePiece checkPiece, GamePiece fromPiece, int xi, int yi){
+        return checkPiece != null && fromPiece.getColor() != checkPiece.getColor() && checkPiece.canMove(new Position(-xi, -yi));
+    }
+
+    private boolean isNowOpponentsTurn(GamePiece piece){
+        return piece.getColor() != getPlayerColor();
+    }
+
+    private boolean isOpponentsPieceChosen(GamePiece fromPiece, GamePiece toPiece){
+        return toPiece != null && fromPiece.getColor() == toPiece.getColor();
+    }
+
+    private boolean isPositionAlreadyTaken(Position pos){
+        return board.getPiece(pos) != null;
+    }
+
+    private boolean isThisPieceAvailable(GamePiece piece){
+        return board.countCapturedPieces(piece) < 1;
+    }
+
+    private boolean isPieceAttacked(GamePiece piece){
+        return piece != null;
+    }
+
+    private boolean isLionReachingEndOfBoard(GamePiece fromPiece, Position to){
+        return fromPiece.getType() == PieceType.LION && (fromPiece.getColor() == PieceColor.BLACK &&
+                to.y == 0 || fromPiece.getColor() == PieceColor.WHITE && to.y == board.getSize().y - 1);
+    }
+
+    private boolean isPieceReachningEndOfBoardAndNotPromoted(GamePiece fromPiece, Position to){
+        return ((to.y == board.getSize().y - 1 && fromPiece.getColor() == PieceColor.WHITE ) ||
+                (to.y == 0 && fromPiece.getColor() == PieceColor.BLACK)) && fromPiece.promote() != null;
+    }
+
+
 }
