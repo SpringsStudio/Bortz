f915bc290864eb085f5415233a06d77c4e77482c
Ashymad <czilukim@o2.pl>
2017-11-20 17:33:18 +0100

    Added listPositions() method.

--- code/app/src/main/java/studio/springs/bortz/engine/utils/Position.java
+++ code/app/src/main/java/studio/springs/bortz/engine/utils/Position.java
@@ -1,5 +1,8 @@
 package studio.springs.bortz.engine.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Position {
     final public int x;
     final public int y;
@@ -18,5 +21,14 @@ public class Position {
     }
     public static Position Add(Position one, Position two){
         return new Position(one.x + two.x, one.y + two.y);
+    }
+    public static List<Position> listPositions(Position from, Position to){
+        List<Position> list = new ArrayList<>(((to.x - from.x + 1) * (to.y - from.y + 1)));
+        for (int xi = from.x; xi < to.x; xi++){
+            for (int yi = from.y; yi < to.y; yi++){
+                list.add(new Position(xi,yi));
+            }
+        }
+        return list;
     }
 }
