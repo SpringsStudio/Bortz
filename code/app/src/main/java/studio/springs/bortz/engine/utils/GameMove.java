74fb9c7038c7d35ca8581ad0499b0d8bea7b5a9e
Ashymad <czilukim@o2.pl>
2017-11-22 23:46:57 +0100

    Add promotion field to GameMovw

--- code/app/src/main/java/studio/springs/bortz/engine/utils/GameMove.java
+++ code/app/src/main/java/studio/springs/bortz/engine/utils/GameMove.java
@@ -12,11 +12,20 @@ public class GameMove {
     final public Position origin;
     final public MoveType movement;
     final public Position destination;
+    final public Boolean promotion;
 
+    public GameMove(PieceType piece, Position origin, MoveType movement, Position destination, Boolean promotion){
+        this.piece = piece;
+        this.origin = origin;
+        this.movement = movement;
+        this.destination = destination;
+        this.promotion = promotion;
+    }
     public GameMove(PieceType piece, Position origin, MoveType movement, Position destination){
         this.piece = piece;
         this.origin = origin;
         this.movement = movement;
         this.destination = destination;
+        this.promotion = null;
     }
 }
