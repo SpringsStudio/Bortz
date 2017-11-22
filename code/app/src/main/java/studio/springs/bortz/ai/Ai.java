a1fe9a0cbcfbbdd4e22ee8605f0e710c3d83c062
Ashymad <czilukim@o2.pl>
2017-11-22 14:12:59 +0100

    Ai.evaluateBoard() now considers the value of captured pieces.

--- code/app/src/main/java/studio/springs/bortz/ai/Ai.java
+++ code/app/src/main/java/studio/springs/bortz/ai/Ai.java
@@ -1,12 +1,14 @@
 package studio.springs.bortz.ai;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import studio.springs.bortz.engine.GameMove;
 import studio.springs.bortz.engine.board.BoardLogic;
 import studio.springs.bortz.engine.board.GameBoard;
 import studio.springs.bortz.engine.pieces.GamePiece;
+import studio.springs.bortz.engine.pieces.GamePieceFactory;
 import studio.springs.bortz.engine.pieces.PieceColor;
 import studio.springs.bortz.engine.pieces.PieceType;
 import studio.springs.bortz.engine.utils.Position;
@@ -17,11 +19,23 @@ public abstract class Ai extends Thread{
     protected BoardLogic logic;
     protected PieceColor aiColor;
     private GameMove calculatedMove;
+    private List<Position> boardPositions;
+    private GamePiece[] capturablePieces = new GamePiece[] {
+            GamePieceFactory.createPiece(PieceType.GIRAFFE, PieceColor.BLACK),
+            GamePieceFactory.createPiece(PieceType.GIRAFFE, PieceColor.WHITE),
+            GamePieceFactory.createPiece(PieceType.ELEPHANT, PieceColor.BLACK),
+            GamePieceFactory.createPiece(PieceType.ELEPHANT, PieceColor.WHITE),
+            GamePieceFactory.createPiece(PieceType.CHICK, PieceColor.BLACK),
+            GamePieceFactory.createPiece(PieceType.CHICK, PieceColor.WHITE)
+    };
 
     Ai(BoardLogic logic, GameBoard board, PieceColor aiColor){
         this.board = board;
         this.aiColor = aiColor;
         this.logic = logic;
+        this.boardPositions = Position.listPositions(
+            new Position(0,0),
+            Position.Subtract(this.board.getSize(),new Position(1,1)));
     }
     private static Map<PieceType,Integer> piecesValuse = new HashMap<>(); static {
         piecesValuse.put(PieceType.LION, 900);
@@ -30,16 +44,21 @@ public abstract class Ai extends Thread{
         piecesValuse.put(PieceType.ELEPHANT, 30);
         piecesValuse.put(PieceType.CHICK, 10);
     }
-    static int evaluateBoard(GameBoard board){
+
+
+    int evaluateBoard(GameBoard board){
         int value = 0;
-        for (Position pos :
-                Position.listPositions(new Position(0,0),
-                        Position.Subtract(board.getSize(),new Position(1,1)))){
+        for (Position pos : boardPositions){
             GamePiece piece = board.getPiece(pos);
             if (piece != null) {
                 value += piecesValuse.get(piece.getType()) *
                         (piece.getColor() == PieceColor.WHITE ? 1 : -1);
             }
+        }
+        for (GamePiece piece : capturablePieces){
+            value += board.countCapturedPieces(piece) *
+                    piecesValuse.get(piece.getType()) * 0.5 *
+                    (piece.getColor() == PieceColor.WHITE ? 1 : -1);
         }
         return value;
     }
